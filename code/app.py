82281479a00b1e16418d7083889a659a378bb2ea
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2020-06-09 15:53:38 -0700

    created item resource with get and post methods

--- code/app.py
+++ code/app.py
@@ -4,12 +4,25 @@ from flask_restful import Resource, Api
 
 # Creates Flask app
 app = Flask(__name__)
+# Creates API for app
 api = Api(app)
 
-class Student(Resource):
-    def get(self,name):
-        return {'student':name}
+items = []
 
-api.add_resource(Student, '/student/<string:name>')
+# Defines Resource
+class Item(Resource):
+    # Defines the methods that the resource accepts
+    def get(self,name):
+        for item in items:
+            if item['name'] == name:
+                return item
+
+    def post(self,name):
+        item = {'name': name, 'price': 12.00}
+        items.append(item)
+        return item
+
+# Adss resource and determines how it's accesed
+api.add_resource(Item, '/item/<string:name>')
 
 app.run()
