777d338e4202ed11ad4ef932d528e4957ca20348
Cesar Jaramillo <csarjaramillo89@yahoo.com>
2020-06-10 14:26:57 -0700

    request parsing with Flask-RESTful

--- code/app.py
+++ code/app.py
@@ -18,9 +18,16 @@ items = []
 
 # Defines Resource
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price',
+        type=float,
+        required=True,
+        help="This field cannot be left blank"
+    )
+
     @jwt_required()
-    # Defines the methods that the resource accepts
     def get(self,name):
+        # Defines the methods that the resource accepts
         # lambda function used to filter through list
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item': item}, 200 if item else 404
@@ -28,26 +35,23 @@ class Item(Resource):
     def post(self,name):
         if next(filter(lambda x: x['name'] == name, items), None):
             return {'message': "An item with '{}' already exists.".format(name)}, 400
-        # get JSON payload from request
-        data = request.get_json()
+
+        data = Item.parser.parse_args()
+
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
 
+    @jwt_required()
     def delete(self, name):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message': 'Item deleted'}
 
+    @jwt_required()
     def put(self, name):
-        parser = reqparse.RequestParser()
-        parser.add_argument('price',
-            type=float,
-            required=True,
-            help="This field cannot be left blank"
-        )
-        data = parser.parse_args()
-        
+        data = Item.parser.parse_args()
+
         item = next(filter(lambda x: x['name'] == name, items), None)
         if item is None:
             item = {'name': name, 'price': data['price']}
